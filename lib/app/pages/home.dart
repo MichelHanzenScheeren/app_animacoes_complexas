9c9c73727896b746226c246c12650e28a1e3bca6
MichelHanzenScheeren <michelhanzenscheeren@gmail.com>
2020-05-05 17:26:17 -0300

    Construção de um GradientContainer e início da construção da tela de login.

--- lib/app/pages/home.dart
+++ lib/app/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:appanimacoescomplexas/app/auxiliarWidgets/gradientContainer.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,6 +9,25 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: GradientContainer(
+        child: Center(
+          child: FractionallySizedBox(
+            widthFactor: 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(),
+                SizedBox(height: 15),
+                TextFormField(),
+                SizedBox(height: 30),
+                RaisedButton(onPressed: (){})
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
